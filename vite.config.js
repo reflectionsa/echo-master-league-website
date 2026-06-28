9dcfb5e6176340a62a2f6d64beea9762cff89326
Spritz <spritz@metis.agents>
2026-06-28 01:54:27 -0400

    fix: remove manualChunks code-split for real (overwrite vite config)

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
-  plugins: [react()],
-  base: '/',
-  server: {
-    port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'chakra-vendor': ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          'particles-vendor': ['@tsparticles/react', '@tsparticles/slim'],
-          icons: ['lucide-react'],
-        },
-      },
-    },
-  },
-}));
+    plugins: [react()],
+    base: '/',
+    server: {
+        port: 3000
+    }
+}))
